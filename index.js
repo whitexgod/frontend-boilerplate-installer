55d672d539d87abd6f9a1449fb33fc43da716d1f
Tuhin Mukherjee <tuhinmukherjee74@gmail.com>
2024-05-25 15:38:29 +0530

    script update

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 #!/usr/bin/env node
 
-const simpleGit = require("simple-git");
-const path = require("path");
-const fs = require("fs");
-const rimraf = require("rimraf");
+const simpleGit = require('simple-git');
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
 
-const repoUrl = "https://github.com/whitexgod/Frontend-Skeleton-Setup.git";
+const repoUrl = 'https://github.com/whitexgod/frontend-skeleton-setup.git';  // Replace with your repo URL
 const targetPath = process.cwd();
 
 const cloneRepo = async () => {
   try {
-    if (fs.existsSync(path.join(targetPath, ".git"))) {
-      console.log("Project already initialized.");
-    } else {
-      console.log("Cloning repository...");
-      await simpleGit().clone(repoUrl, targetPath);
-      console.log("Repository cloned successfully.");
+    console.log('Cloning repository...');
+    await simpleGit().clone(repoUrl, targetPath);
+    console.log('Repository cloned successfully.');
 
-      // Remove the .git directory to disconnect from the original repo
-      rimraf(path.join(targetPath, ".git"), (err) => {
-        if (err) {
-          console.error("Failed to remove .git directory:", err);
-        } else {
-          console.log(
-            ".git directory removed. Disconnected from the original repository."
-          );
-        }
-      });
-    }
+    // Remove the .git directory to disconnect from the original repo
+    rimraf(path.join(targetPath, '.git'), (err) => {
+      if (err) {
+        console.error('Failed to remove .git directory:', err);
+      } else {
+        console.log('.git directory removed. Disconnected from the original repository.');
+      }
+    });
   } catch (err) {
-    console.error("Failed to clone repository:", err);
+    console.error('Failed to clone repository:', err);
   }
 };
 
